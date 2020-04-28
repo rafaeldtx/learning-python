2d597e6a220e864ec2765d6a9ffc0562ee9a1ba7
rafaeldtx <rafael991_@hotmail.com>
2020-04-28 11:38:41 -0300

    (global) Include readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+## LEARNING PYTHON
+
+This repository is used for publish basic programming with python language
--- guess-game/guess.py
+++ guess-game/guess.py
@@ -7,3 +7,8 @@ secret_number = 43
 bet_number = input('Write your number:')
 
 print('Your number is:', bet_number, sep=' ')
+
+if(int(bet_number) == secret_number):
+  print("You're right!!!")
+else:
+  print("You're wrong. Try again")
