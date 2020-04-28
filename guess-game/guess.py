a84d4a0967b6b37e00444cd236834ded562bba9f
rafaeldtx <rafael991_@hotmail.com>
2020-04-28 11:24:28 -0300

    (guess-game) create basic guess file

---
+++ guess-game/guess.py
@@ -0,0 +1,9 @@
+print('***********************')
+print('*******Guess Game******')
+print('***********************')
+
+secret_number = 43
+
+bet_number = input('Write your number:')
+
+print('Your number is:', bet_number, sep=' ')
