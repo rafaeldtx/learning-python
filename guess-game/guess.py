4f0bd8e28b73a2ad55c7afeb122c91fd472aecc2
rafaeldtx <rafael991_@hotmail.com>
2020-04-30 14:55:59 -0300

    (guess-game) using f-string interpolation

--- guess-game/guess.py
+++ guess-game/guess.py
@@ -6,21 +6,23 @@ secret_number = 43
 attempts = 5
 
 while(attempts > 0):
+  print(f'{attempts} tentativas restantes', end="\n\n")
   bet_number = int(input('Write your number:'))
-
-  print(attempts,'tentativas restantes', end="\n\n")
   attempts -= 1
 
-  print('Your number is:', bet_number, sep=' ', end="\n\n")
+  print(f'Your number is: {bet_number}', sep=' ', end="\n\n")
 
   if(bet_number == secret_number):
-    print("You're right!!!")
+    print("Congratulations! You're right!!!")
     break
   else:
     print("You're wrong. Try again", end="\n\n")
     if(bet_number > secret_number):
-      print("TIP: The secret number is below of {}".format(bet_number), end="\n\n")
+      print(f"TIP: The secret number is below of {bet_number}", end="\n\n")
     else:
-      print("TIP: The secret number is above of {}".format(bet_number), end="\n\n")
+      print(f"TIP: The secret number is above of {bet_number}", end="\n\n")
+  print('------------------------------------------', end='\n\n')
+
+
 
 print('End game')
