bba47f49fcf6a59a1db1eb8ba513be03ea708300
rafaeldtx <rafael991_@hotmail.com>
2020-05-02 20:34:00 -0300

    (guess-game) Add difficulty levels

--- guess-game/guess.py
+++ guess-game/guess.py
@@ -2,10 +2,26 @@ import random
 
 print('***********************')
 print('*******Guess Game******')
-print('***********************')
+print('***********************', end='\n\n')
 
 secret_number = random.randrange(1,101)
-attempts = 5
+attempts = None
+
+while attempts == None:
+  print('Inform the difficulty level')
+  print('1- Easy \n2- Medium \n3- Hard')
+  level = int(input('Put the difficulty: '))
+
+  if level == 1:
+    attempts = 12
+  elif level == 2:
+    attempts = 8
+  elif level == 3:
+    attempts = 4
+  else:
+    print('Not valid level. Insert between 1 and 3', end='\n\n')
+  print('------------------------------------------', end='\n\n')
+
 
 while(attempts > 0):
   print(f'{attempts} tentativas restantes', end="\n\n")
@@ -18,11 +34,13 @@ while(attempts > 0):
     print("Congratulations! You're right!!!")
     break
   else:
-    print("You're wrong. Try again", end="\n\n")
+    print("You're wrong.")
     if(bet_number > secret_number):
       print(f"TIP: The secret number is below of {bet_number}", end="\n\n")
-    else:
+    elif(attempts != 0):
       print(f"TIP: The secret number is above of {bet_number}", end="\n\n")
+    else:
+      print(f"You lose! The secret number is {secret_number}")
   print('------------------------------------------', end='\n\n')
 
 
