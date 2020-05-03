220c1eadcc30b21d0cd4ecdca14fa009442c50f4
rafaeldtx <rafael991_@hotmail.com>
2020-05-03 15:49:00 -0300

    (fast-games) add hub file for access games

---
+++ fast-games/hub.py
@@ -0,0 +1,21 @@
+import guess
+import hangman
+
+print('*****************')
+print('****HUB GAMES****')
+print('*****************', end='\n\n')
+
+
+print('Which game do you want to play?')
+print('1- Guess \n2- Hangman', end='\n\n')
+
+game_choice = int(input('Informe the number:'))
+print('------------------------------', end='\n\n')
+
+if game_choice == 1:
+  guess.start()
+elif game_choice == 2:
+  hangman.start()
+else:
+  print('Invalid value. Put one in the list.')
+
