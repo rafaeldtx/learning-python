ec8e91102ddfd3338847ff6f28e6f148ede51c6b
rafaeldtx <rafael991_@hotmail.com>
2020-05-04 17:47:20 -0300

    (hangman-game) add tip for secret word

--- fast-games/hangman.py
+++ fast-games/hangman.py
@@ -3,8 +3,42 @@ def start():
     print('****HANGMAN GAME****')
     print('********************', end='\n\n')
 
-    attempts = None
     bet_letters = []
+    secret_word = 'banana'
+    right_letters = []
+
+    for letter in secret_word:
+        right_letters.append('_')
+
+    attempts = calls_difficulty_attempts()
+
+    while attempts > 0:
+        bet_letters.sort()
+
+        print('--------------------------', end='\n\n')
+        if len(bet_letters):
+            print(f"CHUTES: {', '.join(bet_letters)}", end='\n\n')
+
+        print(f"PALAVRA: {' '.join(right_letters)}", end='\n\n')
+
+        new_letter = input('Digit a new letter:').upper()
+
+        if len(new_letter) != 1 or new_letter.isdigit():
+            print('Wrong value informed. Put only one LETTER.')
+        elif new_letter in bet_letters:
+            print(f'Letter "{new_letter}" was been informed before. Try another one.')
+        else:
+            bet_letters.append(new_letter)
+
+            for index in range(0, len(secret_word)):
+                secret_letter = secret_word[index].upper()
+
+                if new_letter == secret_letter:
+                    right_letters[index] = secret_letter
+
+
+def calls_difficulty_attempts():
+    attempts = None
 
     while attempts == None:
         print('Inform the difficulty level')
@@ -20,20 +54,7 @@ def start():
         else:
             print('Not valid level. Insert between 1 and 3', end='\n\n')
 
-        while attempts > 0:
-            bet_letters.sort()
-            print('--------------------------', end='\n\n')
-            if len(bet_letters):
-                print(', '.join(bet_letters))
-
-            new_letter = input('Digit a new letter:').upper()
-
-            if len(new_letter) != 1 or new_letter.isdigit():
-                print('Wrong value informed. Put only one LETTER.')
-            elif new_letter in bet_letters:
-                print(f'Letter "{new_letter}" was been informed before. Try another one.')
-            else:
-                bet_letters.append(new_letter)
+    return attempts
 
 if(__name__ == "__main__"):
     start()
