3a3d5b7c931326861884f21460f535960938d9ef
rafaeldtx <rafael991_@hotmail.com>
2020-05-05 21:13:39 -0300

    (hangman) refactor logical structure

--- fast-games/hangman.py
+++ fast-games/hangman.py
@@ -1,9 +1,9 @@
 import random
 
 def start():
-    secret_word = random.choice(secret_words_list())
+    secret_word = random.choice(get_secret_words_list()).upper()
     right_letters = ["_" for letter in secret_word]
-    attempts = get_difficulty_attempts()
+    attempts = set_difficulty_attempts(len(secret_word))
     bet_letters = []
 
     print('********************')
@@ -14,22 +14,33 @@ def start():
         show_bet_letters = ', '.join(bet_letters)
 
         print('--------------------------', end='\n\n')
-        print(f"CHUTES: {show_bet_letters}")
-        print(f"PALAVRA: {' '.join(right_letters)}", end='\n\n')
+        print(f"You have {attempts} attempt(s) remaining.", end='\n\n')
+        print(f"BETS: {show_bet_letters}")
+        print(f"WORD: {' '.join(right_letters)}", end='\n\n')
 
         new_bet_letter = input('Digit a new letter:').strip().upper()
 
-        if len(new_bet_letter) != 1 or new_bet_letter.isdigit():
-            print('Wrong value informed. Put only one LETTER.')
-        elif new_bet_letter in bet_letters:
-            print(f'Letter "{new_bet_letter}" was been informed before. Try another one.')
-        else:
+        if right_bet(new_bet_letter, bet_letters):
+            if new_bet_letter not in secret_word:
+                attempts -= 1
+
             bet_letters.append(new_bet_letter)
             bet_letters.sort()
 
-            get_right_letters(secret_word, new_bet_letter, right_letters)
+            right_letters = set_right_letters(secret_word, new_bet_letter, right_letters)
+        else:
+            if new_bet_letter in bet_letters:
+                print(f'Letter "{new_bet_letter}" was been informed before. Try another one.')
+            else:
+                print('Wrong value informed. Put only one LETTER.')
 
-def get_difficulty_attempts():
+        if '_' not in right_letters:
+            print(f'Congratulations! You right the secret word: "{secret_word}"!')
+            break
+        elif attempts == 0:
+            print(f'You Lose! The secrect word was {secret_word}')
+
+def set_difficulty_attempts(word_length):
     attempts = None
 
     while attempts == None:
@@ -38,17 +49,17 @@ def get_difficulty_attempts():
         level = int(input('Put the difficulty: '))
 
         if level == 1:
-            attempts = 12
+            attempts = round(2.5 * word_length)
         elif level == 2:
-            attempts = 8
+            attempts = round(2.0 * word_length)
         elif level == 3:
-            attempts = 4
+            attempts = round(1.5 * word_length)
         else:
             print('Not valid level. Insert between 1 and 3', end='\n\n')
 
     return attempts
 
-def get_right_letters(secret_word, new_bet_letter, right_letters):
+def set_right_letters(secret_word, new_bet_letter, right_letters):
     for index in range(0, len(secret_word)):
         secret_letter = secret_word[index].upper()
 
@@ -57,7 +68,7 @@ def get_right_letters(secret_word, new_bet_letter, right_letters):
 
     return right_letters
 
-def secret_words_list():
+def get_secret_words_list():
     return [
         'apple',
         'avocado',
@@ -69,6 +80,13 @@ def secret_words_list():
         'pumpkin',
         'tomato',
     ]
+
+def right_bet(new_bet, bet_list):
+    return(
+        len(new_bet) == 1 and
+        not new_bet.isdigit() and
+        new_bet not in bet_list
+    )
 
 if(__name__ == "__main__"):
     start()
