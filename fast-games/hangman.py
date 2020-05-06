bcfdeb6024c272242198f09454bf52a8afafeccf
rafaeldtx <rafael991_@hotmail.com>
2020-05-05 22:05:51 -0300

    (hangman) add with for open/close file

--- fast-games/hangman.py
+++ fast-games/hangman.py
@@ -69,8 +69,8 @@ def set_right_letters(secret_word, new_bet_letter, right_letters):
     return right_letters
 
 def get_secret_words_list():
-    secret_words_file = open("secret_words.txt", "r")
-    return [word_line.strip() for word_line in secret_words_file]
+    with open("secret_words.txt", "r") as secret_words_file:
+        return [word_line.strip() for word_line in secret_words_file]
 
 def right_bet(new_bet, bet_list):
     return(
