46142551ea322e579910eb592f416a1e2b577ea9
rafaeldtx <rafael991_@hotmail.com>
2020-05-06 15:56:29 -0300

    (OO) Update object to receive dynamic data

--- object-oriented/bank_account.py
+++ object-oriented/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
-    def __init__(self):
-        self.number = 123
-        self.user = "Nico"
-        self.amount = 150.00
-        self.limit = 100.00
+    def __init__(self, owner, number, amount, limit):
+        self.number = number
+        self.owner = owner
+        self.amount = amount
+        self.limit = limit
