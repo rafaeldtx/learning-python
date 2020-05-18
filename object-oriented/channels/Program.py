61b8a701a5301a9081658abf1b94a29d58bbc8af
rafaeldtx <rafael991_@hotmail.com>
2020-05-18 11:57:02 -0300

    (OO) print textual object

---
+++ object-oriented/channels/Program.py
@@ -0,0 +1,40 @@
+class Program:
+    def __init__(self, name, year):
+        self._name = name.title()
+        self.year = year
+        self._likes = 0
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name.title()
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @likes.setter
+    def likes(self, likes):
+        self._likes = likes.title()
+
+    def __str__(self):
+        return f'Name: {self.name} - Year: {self.year} - Likes: {self.likes}'
+
+class Film(Program):
+    def __init__(self, name, year, duration):
+        super().__init__(name, year)
+        self.duration = duration
+
+    def __str__(self):
+        return f'Name: {self.name} - Year: {self.year} - Likes: {self.likes} - Duration: {self.duration}min'
+
+class Serie(Program):
+    def __init__(self, name, year, seasons):
+        super().__init__(name, year)
+        self.seasons = seasons
+
+    def __str__(self):
+        return f'Name: {self.name} - Year: {self.year} - Likes: {self.likes} - Seasons: {self.seasons}'
