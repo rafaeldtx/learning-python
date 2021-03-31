01c006b14aa5d6f513a24ee21677a71e31479b80
Rafael Domingues Teixeira <rafael991_@hotmail.com>
2021-03-31 20:58:23 -0300

    (aula1) Using comments

---
+++ 1_python_basico/aula1.py
@@ -0,0 +1,13 @@
+# Aula 1 - Uso de comentários
+
+"""
+DOC STRINGS
+
+Funcionamento semelhante a comentário, mas é utilizado para documentação
+"""
+
+'''
+Possível utilizar aspas duplas e aspas simples para seção de comentários
+'''
+
+print('Hello World!')
