c303e94dfddb257adc60078a89c45a709440f08b
Rafael Domingues Teixeira <rafael991_@hotmail.com>
2021-03-31 20:51:58 -0300

    (old files) removed from main

--- object-oriented/channels/Film.py
+++
@@ -1,9 +0,0 @@
-from Program import Program
-
-class Film(Program):
-    def __init__(self, name, year, duration):
-        super().__init__(name, year)
-        self.duration = duration
-
-    def __str__(self):
-        return f'Name: {self.name} - Year: {self.year} - Likes: {self.likes} - Duration: {self.duration}min'
--- object-oriented/channels/Serie.py
+++
@@ -1,9 +0,0 @@
-from Program import Program
-
-class Serie(Program):
-    def __init__(self, name, year, seasons):
-        super().__init__(name, year)
-        self.seasons = seasons
-
-    def __str__(self):
-        return f'Name: {self.name} - Year: {self.year} - Likes: {self.likes} - Seasons: {self.seasons}'
