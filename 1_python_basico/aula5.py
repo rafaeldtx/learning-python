232b1a2f57ab2271e96fffd8bb91e52950f35a30
Rafael Domingues Teixeira <rafael991_@hotmail.com>
2021-03-31 22:45:46 -0300

    (aula5) arithmetic operators

---
+++ 1_python_basico/aula5.py
@@ -0,0 +1,16 @@
+''' AULA 5 - Operadores aritméticos
++ Adição
+- Subtração
+* Multiplicação
+/ Divisão (valor real)
+// Divisão (valor inteiro)
+** exponenciação
+% resto da divisão
+'''
+print('Adição:',10+10) # -> 20
+print(10-3) # -> 7
+print(10*3) # -> 30
+print(10/3) # -> 3.3333333333333335
+print(10//3) # -> 3
+print(10**3) # -> 1000
+print(10%3) # -> 0
