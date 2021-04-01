7a45521bc482215c6c62fff0f87e8fe01e30b429
Rafael Domingues Teixeira <rafael991_@hotmail.com>
2021-03-31 21:59:07 -0300

    (Aula3) printing strings

---
+++ 1_python_basico/aula3.py
@@ -0,0 +1,17 @@
+# AULA 3 - Strings e aspas em Python
+
+# String ou str - Tipo de dado primitivo em Python, consiste em ser uma
+# cadeia de caracteres
+
+# Tudo em Python é um objeto, incluindo tipos primitivos
+
+# Python é uma linguagem de tipagem dinâmica
+
+print('uso de aspas simples na inicialização "exemplo de string"')
+print("uso de aspas duplas na inicialização 'exemplo de string'")
+print("uso de caracter de escape \"exemplo de string\"")
+
+# Uso de Raw em strings
+# Ao utilizar "r" (raw) no anteriormente ao início da string informa ao
+# Python que o texto será lido de forma literal
+print(r'exibindo caracter de escape: \n - \' - \"')
