4d397a54f675498bd7860dcedde96769295ca6f6
Rafael Domingues Teixeira <rafael991_@hotmail.com>
2021-03-31 22:53:53 -0300

    (aula6) Variables

---
+++ 1_python_basico/aula6.py
@@ -0,0 +1,9 @@
+# AULA6 - Variaveis
+#  - Devem sempre iniciar com letras
+#  - Podem conter números
+#  - separados por _
+#  - Minusculas
+
+full_name = 'Rafael Domingues Teixeira'
+
+print(full_name, type(full_name))
